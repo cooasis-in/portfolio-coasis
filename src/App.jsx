7184a6e905f21672388ff7b85a4bf01b0244ad4b
shahbaz28 <shahbaz.khan2865@gmail.com>
2024-09-07 16:17:06 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import viteLogo from "/vite.svg";
 import "./App.css";
 import TalentPoolDesign from "./components/TalentPoolDesign";
+import WorkShowcase from "./components/WorkShowcase";
 
 function App() {
 
   return (
     <>
+    <WorkShowcase/>
     <TalentPoolDesign/>
     </>
   );
