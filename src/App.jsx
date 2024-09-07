2ee56f6b14663b3ff1f558243bbb79d3b5bf2a1d
shahbaz28 <shahbaz.khan2865@gmail.com>
2024-09-07 15:22:26 +0530

    CreatedUi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import viteLogo from "/vite.svg";
 import "./App.css";
+import TalentPoolDesign from "./components/TalentPoolDesign";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <TalentPoolDesign/>
     </>
   );
 }
