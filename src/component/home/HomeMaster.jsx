6aeaf3711334d4a75458aa83a3545d7854de7c37
rishiirajanand <rishirajanand5@gmail.com>
2024-09-10 11:39:18 +0530

    merge with shahbaz

--- src/component/home/HomeMaster.jsx
+++ src/component/home/HomeMaster.jsx
@@ -10,7 +10,7 @@ const HomeMaster = () => {
   return (
     <div>
       {/* <DotGrid/> */}
-      <WaterDropGrid />
+      {/* <WaterDropGrid /> */}
       <Hero />
       <CommunityDesigner />
       <WorkShowcase />
